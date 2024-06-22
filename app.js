b58431d6952c39efcb21c49985b6dbfd60e609d0
dosnet <dosnet2200@gmail.com>
2024-06-23 00:32:14 +0300

    updated

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const tasks = require('./routes/tasks');
+const auth = require('./routes/auth');
 const app = express();
-const PORT = 3000;
+
+require('dotenv').config();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(auth);
 app.use(tasks);
 
 app.use((err, req, res, next) => {
